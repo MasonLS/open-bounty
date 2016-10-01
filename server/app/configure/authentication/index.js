b38f7b0101d9efc6767e40725a3ef1646ba5558f
devpatel91 <devnitinpatel91@gmail.com>
2016-09-30 23:01:42 -0400

    OAuth is working

--- server/app/configure/authentication/index.js
+++ server/app/configure/authentication/index.js
@@ -1,76 +1,74 @@
-'use strict';
-var path = require('path');
-var session = require('express-session');
-var passport = require('passport');
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
+'use strict'
+var path = require('path')
+var session = require('express-session')
+var passport = require('passport')
+var SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 var ENABLED_AUTH_STRATEGIES = [
-    'local',
-    //'twitter',
-    //'facebook',
-    //'google'
-];
+  'local',
+  // 'twitter',
+  'github',
+// 'google'
+]
 
 module.exports = function (app, db) {
+  var dbStore = new SequelizeStore({
+    db: db
+  })
 
-    var dbStore = new SequelizeStore({
-        db: db
-    });
+  var User = db.model('user')
 
-    var User = db.model('user');
+  dbStore.sync()
 
-    dbStore.sync();
+  // First, our session middleware will set/read sessions from the request.
+  // Our sessions will get stored in Mongo using the same connection from
+  // mongoose. Check out the sessions collection in your MongoCLI.
+  app.use(session({
+    secret: app.getValue('env').SESSION_SECRET,
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
+  }))
 
-    // First, our session middleware will set/read sessions from the request.
-    // Our sessions will get stored in Mongo using the same connection from
-    // mongoose. Check out the sessions collection in your MongoCLI.
-    app.use(session({
-        secret: app.getValue('env').SESSION_SECRET,
-        store: dbStore,
-        resave: false,
-        saveUninitialized: false
-    }));
+  // Initialize passport and also allow it to read
+  // the request session information.
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-    // Initialize passport and also allow it to read
-    // the request session information.
-    app.use(passport.initialize());
-    app.use(passport.session());
+  // When we give a cookie to the browser, it is just the userId (encrypted with our secret).
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
 
-    // When we give a cookie to the browser, it is just the userId (encrypted with our secret).
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+  // When we receive a cookie from the browser, we use that id to set our req.user
+  // to a user found in the database.
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .then(user => {
+        done(null, user)
+      })
+      .catch(done)
+  })
 
-    // When we receive a cookie from the browser, we use that id to set our req.user
-    // to a user found in the database.
-    passport.deserializeUser((id, done) => {
-        User.findById(id)
-            .then(user => {
-                done(null, user);
-            })
-            .catch(done);
-    });
+  // We provide a simple GET /session in order to get session information directly.
+  // This is used by the browser application (Angular) to determine if a user is
+  // logged in already.
+  app.get('/session', (req, res) => {
+    if (req.user) {
+      res.send({ user: req.user.sanitize() })
+    } else {
+      res.status(401).send('No authenticated user.')
+    }
+  })
 
-    // We provide a simple GET /session in order to get session information directly.
-    // This is used by the browser application (Angular) to determine if a user is
-    // logged in already.
-    app.get('/session', (req, res) => {
-        if (req.user) {
-            res.send({ user: req.user.sanitize() });
-        } else {
-            res.status(401).send('No authenticated user.');
-        }
-    });
+  // Simple /logout route.
+  app.get('/logout', (req, res) => {
+    req.logout()
+    res.status(200).end()
+  })
 
-    // Simple /logout route.
-    app.get('/logout', (req, res) => {
-        req.logout();
-        res.status(200).end();
-    });
-
-    // Each strategy enabled gets registered.
-    ENABLED_AUTH_STRATEGIES.forEach(strategyName => {
-        require(path.join(__dirname, strategyName))(app, db);
-    });
-
-};
+  // Each strategy enabled gets registered.
+  ENABLED_AUTH_STRATEGIES.forEach(strategyName => {
+    require(path.join(__dirname, strategyName))(app, db)
+  })
+}
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,61 +1,61 @@
-'use strict';
-var crypto = require('crypto');
-var _ = require('lodash');
-var Sequelize = require('sequelize');
+'use strict'
+var crypto = require('crypto')
+var _ = require('lodash')
+var Sequelize = require('sequelize')
 
-var db = require('../_db');
+var db = require('../_db')
 
 module.exports = db.define('user', {
-    email: {
-        type: Sequelize.STRING
-    },
-    password: {
-        type: Sequelize.STRING
-    },
-    salt: {
-        type: Sequelize.STRING
-    },
-    twitter_id: {
-        type: Sequelize.STRING
-    },
-    facebook_id: {
-        type: Sequelize.STRING
-    },
-    google_id: {
-        type: Sequelize.STRING
-    }
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  salt: {
+    type: Sequelize.STRING
+  },
+  twitter_id: {
+    type: Sequelize.STRING
+  },
+  github_id: {
+    type: Sequelize.STRING
+  },
+  google_id: {
+    type: Sequelize.STRING
+  }
 }, {
-    instanceMethods: {
-        sanitize: () => {
-            return _.omit(this.toJSON(), ['password', 'salt']);
-        },
-        correctPassword: function (candidatePassword) {
-            return this.Model.encryptPassword(candidatePassword, this.salt) === this.password;
-        }
+  instanceMethods: {
+    sanitize: () => {
+      return _.omit(this.toJSON(), ['password', 'salt'])
     },
-    classMethods: {
-        generateSalt: () => {
-            return crypto.randomBytes(16).toString('base64');
-        },
-        encryptPassword: function (plainText, salt) {
-            var hash = crypto.createHash('sha1');
-            hash.update(plainText);
-            hash.update(salt);
-            return hash.digest('hex');
-        }
-    },
-    hooks: {
-        beforeCreate: user => {
-            if (user.changed('password')) {
-                user.salt = user.Model.generateSalt();
-                user.password = user.Model.encryptPassword(user.password, user.salt);
-            }
-        },
-        beforeUpdate: user => {
-            if (user.changed('password')) {
-                user.salt = user.Model.generateSalt();
-                user.password = user.Model.encryptPassword(user.password, user.salt);
-            }
-        }
+    correctPassword: function (candidatePassword) {
+      return this.Model.encryptPassword(candidatePassword, this.salt) === this.password
     }
-});
+  },
+  classMethods: {
+    generateSalt: () => {
+      return crypto.randomBytes(16).toString('base64')
+    },
+    encryptPassword: function (plainText, salt) {
+      var hash = crypto.createHash('sha1')
+      hash.update(plainText)
+      hash.update(salt)
+      return hash.digest('hex')
+    }
+  },
+  hooks: {
+    beforeCreate: user => {
+      if (user.changed('password')) {
+        user.salt = user.Model.generateSalt()
+        user.password = user.Model.encryptPassword(user.password, user.salt)
+      }
+    },
+    beforeUpdate: user => {
+      if (user.changed('password')) {
+        user.salt = user.Model.generateSalt()
+        user.password = user.Model.encryptPassword(user.password, user.salt)
+      }
+    }
+  }
+})
