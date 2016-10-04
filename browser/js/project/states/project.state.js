fe5afd88293f8b29281f19e17e0e01d45a9f7960
Francesco Saitta <fsaitta@aibu.it>
2016-10-04 11:24:01 -0400

    fixes to project sidebar

--- browser/js/project/states/project.state.js
+++ browser/js/project/states/project.state.js
@@ -6,6 +6,12 @@ app.config(function($stateProvider) {
         templateUrl: 'js/project/templates/project-fund.template.html'
     });
 
+    $stateProvider.state('fundProjectLogged', {
+        url: '/project/fund',
+        controller: 'FundProjectLoggedCtrl',
+        templateUrl: 'js/project/templates/project-fund-logged.template.html'
+    });
+
     $stateProvider.state('manageProjects', {
         url: '/project/manage',
         controller: 'ManageProjectCtrl',
