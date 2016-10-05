9244d0cef6f073bfe2055edff591e8c836c5da96
jeff-bruemmer <jeffbruemmer@gmail.com>
2016-10-05 13:14:20 -0400

    /one/:projectId test

--- tests/server/routes/projects/projects.test.js
+++ tests/server/routes/projects/projects.test.js
@@ -1,64 +1,62 @@
-// app
+'use strict'
+
 const db = require('../../../../server/db');
 const Sequelize = require('sequelize');
+const express = require('express');
+const projectsRouter = require('../../../../server/app/routes/projects');
 
 // testing tools
 const expect = require('chai').expect;
 const supertest = require('supertest');
 
+
 describe('Project Routes', () => {
-    let app, Project;
-    let agent;
+    let Project,
+	agent,
+	testApp;
 
     beforeEach('Sync database', () => db.sync({
         force: true
     }));
 
     beforeEach('Create app', () => {
-        app = require('../../../../server/app')(db);
         Project = db.model('project');
     });
 
-    beforeEach('Create guest agent', () => {
-        agent = supertest.agent(app);
+    beforeEach('Summon test agent', () => {
+        testApp = express();
+        testApp.use((req, res, next) => {
+            req.github = {
+                repos: {
+                    getById: () => Promise.resolve({
+                        id: 1
+                    })
+                }
+            }
+            next();
+        });
+        testApp.use('/api/projects', projectsRouter);
+        agent = supertest.agent(testApp);
     });
 
     const exampleProject = {
-        title: 'Example-Project',
-	repoId: 1,
+        repoId: 2,
         description: 'This is some serious project.',
-        raised: 100,
-	paidout: 50,
-	ownerId: 0
+        raised: 1000,
+        paidout: 500,
+        ownerId: 1
     };
 
-    beforeEach('Create a review', done => {
-        Project.create(exampleProject)
-            .then(done);
-    });
+    beforeEach('create a project', () => Project.create(exampleProject));
 
     it('should get project by id', done => {
         agent.get('/api/projects/one/1')
             .expect(200)
             .end((err, response) => {
                 if (err) done(err);
-                expect(response.body.title).to.equal(exampleProject.title);
-                expect(response.body.description).to.equal(exampleProject.description);
+                expect(response.body.project.raised).to.equal(exampleProject.raised);
+                expect(response.body.project.description).to.equal(exampleProject.description);
                 done();
             });
     });
-
-    // it('should update reviews by id', done => {
-    //     agent.put('/api/reviews/1')
-    //         .send({
-    //             title: 'I changed my mind'
-    //         })
-    //         .expect(200)
-    //         .end((err, response) => {
-    //             if (err) done(err);
-    //             expect(response.body.title).to.equal('I changed my mind');
-    //             expect(response.body.content).to.equal(review.content);
-    //             done();
-    //         });
-    // });
 });
