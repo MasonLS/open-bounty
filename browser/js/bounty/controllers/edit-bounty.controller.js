0fa855f42e4661e156724061a11f88cad287c1d5
jeff-bruemmer <jeffbruemmer@gmail.com>
2016-10-11 17:13:44 -0400

    pre-merge

--- browser/js/bounty/controllers/edit-bounty.controller.js
+++ browser/js/bounty/controllers/edit-bounty.controller.js
@@ -1,9 +1,15 @@
 app.controller('EditBountyCtrl', ($scope, $state, project, ProjectsFactory, BountyFactory, $stateParams) => {
     $scope.project = project;
-    $scope.bountyAmount = project.bounties.filter(bounty => bounty.id === + $stateParams.bountyId)[0].amount;
-    console.log('bountyAmount:',$scope.bountyAmount)
-	       
+    $scope.bountyId = +$stateParams.bountyId
+    $scope.bountyAmount = project.bounties.filter(bounty => bounty.id === +$stateParams.bountyId)[0].amount;
     $scope.issue = project.bounties.filter(bounty => bounty.id === +$stateParams.bountyId)[0].issue;
     $scope.changeBounty = (bountyData) => BountyFactory.updateBounty($stateParams.bountyId, bountyData)
-        .then(updatedBounty => $state.go('singleProject', {projectId: $stateParams.projectId}))
+        .then(updatedBounty => $state.go('singleProject', {
+            projectId: $stateParams.projectId
+        }));
+
+    $scope.deleteBounty = bountyId => BountyFactory.deleteOne(bountyId)
+	.then(() => $state.go('singleProject', {
+	    projectId: $stateParams.projectId
+	}));
 });
