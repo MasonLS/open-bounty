8a3711b52439177eb8d6a8c79b35579950d0cc59
jeff-bruemmer <jeffbruemmer@gmail.com>
2016-10-17 10:12:06 -0400

    fixed sidebar directive

--- browser/js/sidebar/sidebar.directive.js
+++ browser/js/sidebar/sidebar.directive.js
@@ -1,21 +1,22 @@
-app.directive('sidebar', function () {
-  return {
-    restrict: 'E',
-    templateUrl: '/js/sidebar/sidebar.html',
-    // scope: true,
-    controller: function ($scope, ProjectsFactory, BountyFactory) {
-    	$scope.show = 'search';
+app.directive('sidebar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/js/sidebar/sidebar.html',
+        // scope: true,
+        controller: function($scope, ProjectsFactory, BountyFactory) {
+            $scope.show = 'search';
 
-        $scope.$on('bounty taken', (e, bounty) => {
-            $scope.userBounties.push(bounty);
-        })
+            $scope.$on('bounty taken', (e, bounty) => {
+                $scope.userBounties.push(bounty);
+            })
 
-        $scope.$on('bounty untaken', (e, bounty) => {
-            _.pull($scope.userBounties, bounty)
-        })
+            $scope.$on('bounty untaken', (e, bounty) => {
+                _.pull($scope.userBounties, bounty)
+            })
 
-        $scope.$on('project added', (e, project) => {
-            $scope.userProjects.push(project);
-        })
+            $scope.$on('project added', (e, project) => {
+                $scope.userProjects.push(project);
+            })
+        }
     }
 });
